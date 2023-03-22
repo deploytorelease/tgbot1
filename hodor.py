204250b3ff0c124aade19ab14680b1bc8293fb17
Andrew I <andrey.ilyinikh@ro.charity>
2023-03-22 19:27:48 +0300

    fix token

--- hodor.py
+++ hodor.py
@@ -2,7 +2,6 @@ import logging
 import random
 import string
 import os
-from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     Updater,
